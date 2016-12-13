a4364c99d1e901eb9dd1c9340b5797217e887aa7
Can Berk Güder <cbguder@pivotal.io>
2016-12-13 14:52:59 -0800

    Add task to build binaries
    Signed-off-by: aram price <aprice@pivotal.io>

---
+++ ci/build-binaries/task.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eux
+
+export GOPATH=$(pwd)/go
+export PATH=$GOPATH/bin:$PATH
+
+OUTPUT=$(pwd)/build-binaries-output
+
+VERSION=$(cat version/number)
+
+echo "$VERSION" > "${OUTPUT}/name"
+echo "$VERSION" > "${OUTPUT}/tag"
+
+pushd "${GOPATH}/src/github.com/pivotal-cf/reconfigure-pipeline"
+  git rev-parse HEAD > "${OUTPUT}/commit"
+
+  go get -v -t ./...
+
+  for GOOS in linux darwin; do
+    go build
+
+    tar -cvzf "${OUTPUT}/reconfigure-pipeline-${GOOS}.tar.gz" "reconfigure-pipeline"
+  done
+popd
